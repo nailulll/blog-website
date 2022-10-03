f86fdcd12a2c94943002f90cd3ec53afcc143818
Nailul Ngafwa <fakea4988@gmail.com>
2022-10-03 18:40:04 +0700

    init changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = ({ props }) => {
   return (
     <div className="hero py-32">
       <div className="hero-content flex-col lg:flex-row gap-x-10">
-        <img src={props.img} alt="hero" className="max-w-sm" />
+        <img src={props.img} alt="hero" />
         <div>
           <h1 className="text-5xl font-bold">{props.title}</h1>
           <p className="py-12">{props.content}</p>
@@ -14,7 +14,7 @@ const Hero = ({ props }) => {
               <Button>Lihat Berita</Button>
             </Link>
           ) : (
-            <div className="flex items-center gap-x-5">
+            <div className="flex flex-wrap items-center gap-5">
               {props.icon.map((icon) => {
                 return (
                   <div className="flex items-center gap-x-1" key={icon.url}>
--- src/utils/hero.js
+++
@@ -1,2 +0,0 @@
-const hero = [];
-export default hero;
